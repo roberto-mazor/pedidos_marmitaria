4ec22e989cab1478e45dff7913017121bc65c3f5
roberto-mazor <mazor.estudo@gmail.com>
2025-11-14 20:49:23 -0300

    Feat: Incluindo rotas

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const express =requiere ("express")
+const express = require ("express");
+const app = express();
+app.use(express.json());
+
+const cardapio = [
+    "bananaa", "maçã"
+]
+
+app.get("/", function (req,res){
+    res.send("Olá mundo!")
+})
+
+app.get("/cardapio", (req,res) => {
+    res.send(cardapio)
+})
+
+app.get("/cardapio/:id", (req,res) => {
+    const id = req.params.id
+    res.send(cardapio[id -1])
+})
+
+
+app.listen(3000)
